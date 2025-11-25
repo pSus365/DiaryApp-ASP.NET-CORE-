66bbd2023999b8c687482c0ecb2e22883d117673
pSus365 <suotr12@gmail.com>
2025-11-25 21:02:21 +0100

    Dodaj pliki projektów.

---
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DiaryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        // 4 Steps to add a table
+        // 1 Create a Model Class
+        // 2 Add DB Set
+        // 3 add-migration AddDiaryEntryTable
+        // 4 update-database
+
+        public DbSet<DiaryEntry> DiaryEntries { get; set; } // entity framework core part 
+    }
+}
---
+++ DiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    // model wpisu do bazy danych do tabeli 
+    public class DiaryEntry
+    {   // each one will be separete column in the database
+
+        //[Key] it is covered 
+        public int Id { get; set; } // it should be "Id" to be converted by .net core or DiaryEntryId (only these two options)
+        [Required] // title has to have a value - we have nulable true so this is important here
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Now; // init of this value with current time
+    }
+}
