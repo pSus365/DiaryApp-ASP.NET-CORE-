a3afc14b628edf02bd33dbc8d0de94725cc437d4
pSus365 <suotr12@gmail.com>
2025-11-26 15:39:39 +0100

    DELETE function added | covered all CRUD for Diary Entries

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -83,6 +83,35 @@ namespace DiaryApp.Controllers
             return View(obj);
         }
 
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+
+            DiaryEntry? diaryEntry = _db.DiaryEntries.Find(id);
+
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(diaryEntry);
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(DiaryEntry obj)
+        {
+            _db.DiaryEntries.Remove(obj); // remove the  diary entry to the databse
+            _db.SaveChanges(); // saving changes to the database
+
+            return RedirectToAction("Index");
+        }
+
 
     }
 }
