ff333328f3509d69f80d5c4d09523c6091604f0d
pSus365 <suotr12@gmail.com>
2025-11-26 14:54:14 +0100

    client side validation in Diary Enries Added

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -9,6 +9,7 @@ namespace DiaryApp.Models
         //[Key] it is covered 
         public int Id { get; set; } // it should be "Id" to be converted by .net core or DiaryEntryId (only these two options)
         [Required] // title has to have a value - we have nulable true so this is important here
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "The title is too short! (min 3 chars)")] // max is 100 - client side validation
         public string Title { get; set; }
         [Required]
         public string Content { get; set; }
